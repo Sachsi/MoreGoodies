6888e8deaf82cc683e0a08fad57ebcee35e6951e
Sachsi <tobisachse27@msn.com>
2024-12-02 06:51:48 +0100

    function complite

--- Templates/src/main.cpp
+++ Templates/src/main.cpp
@@ -21,24 +21,36 @@ int ArraySum(const int *pArr, size_t arrSize)
 
 int Max(const int* pArr, size_t arrSize)
 {
-    int max = 0;
+    int max = *pArr;
     const int* ptemp = pArr;
     for(int i = 0; i < arrSize; i++)
     {
-        
+        max = *ptemp > max ? *ptemp : max;
+        ptemp++;
     }
     return max;
 }
 
 std::pair<int, int> MinMax(const int* pArr, size_t arrSize)
 {
+    int max = 0;
+    int min = *pArr;
+    const int* ptemp = pArr;
+    max = Max(pArr, arrSize);
+    
+    for(int i = 0; i < arrSize; i++)
+    {
+        min = *ptemp < min ? *ptemp : min;
+        ptemp++;
+    }
 
+    return std::pair<int, int>(min, max);
 }
 
 int main(void)
 {
 
-    std::array<int, 3> arr{15, 16, 19};
+    std::array<int, 3> arr{15, 22, -1};
     int* pArr = arr.begin();
 
     int sum = Add(19, 19);
@@ -46,4 +58,11 @@ int main(void)
 
     int elements = ArraySum(pArr, arr.size());
     std::cout << "Num Elements: " << elements << std::endl;
+
+    int max = Max(pArr, arr.size());
+    std::cout << "Max value ist: " << max << std::endl;
+
+    std::pair<int, int> a = MinMax(pArr, arr.size());
+    std::cout << "Min value: " << a.first << " Max value: " << a.second << std::endl;
+
 }
