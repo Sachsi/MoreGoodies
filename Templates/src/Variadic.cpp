b61228bd8b55fea352bb9295f8c5bbb711f497da
Sachsi <tobisachse27@msn.com>
2024-12-28 16:36:28 +0100

    Variadic Part I

---
+++ Templates/src/Variadic.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+//this problem is, the type of the arguments have to be the same
+// template<typename T>
+// void Print(std::initializer_list<T> args)
+// {
+//     for(const auto &x : args)
+//     {
+//         std::cout << x << std::endl;
+//     }
+// } 
+
+//function is needed to do the last step by reading the
+//arguments of the parameter pack Print(...)
+void Print()
+{
+
+}
+
+//variadic Template parameters pack
+template<typename T, typename...Params>
+//Function parameter pack
+void Print(T a, Params...args)
+{
+    Print(args...);
+}
+
+#ifdef VARIADIC
+int main(void)
+{
+    Print(1, 2, 3, 4);
+    return 1;
+}
+#endif
