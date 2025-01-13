8cd7445a5b8be539099a8adbda0a41977398dc78
Tobias Sachse <tobias.sachse@rohde-schwarz.com>
2025-01-13 10:14:34 +0100

    Alaise done

--- TypedefAlias/src/main.cpp
+++ TypedefAlias/src/main.cpp
@@ -6,6 +6,23 @@ const char * GetErrorMessage(int errorNo){
     return "Empty";
 }
 
-void ShowError(const char *(*pfn)(int)){
+//typedef declaration
+//typedef const char *(*PFN)(int);
+//or using Alias 
+using PFN = const char *(*)(int);
+//void ShowError(const char *(*pfn)(int)){
+void ShowError(PFN pfn){
     
+}
+
+typedef std::vector<std::list<std::string>> Names;
+//Aliase of long type declaration, lock better
+using Name = std::vector<std::list<std::string>>;
+
+int main(){
+    Names namen;
+    Name nname;
+    PFN pfn = GetErrorMessage;
+    ShowError(pfn);
+    return 0;
 }
