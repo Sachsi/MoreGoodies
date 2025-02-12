697df8b1ef2f3a7c60ef7c251269e9075cc7e43e
Tobias Sachse <tobias.sachse@rohde-schwarz.com>
2025-02-12 08:28:40 +0100

    std::binding PART III

--- std_Library/src/bind.cpp
+++ std_Library/src/bind.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <functional>
 
+//#define PART_2
+#define PART_3
+
 int Subtract(int x, int y){
     return x - y;
 }
@@ -31,6 +34,7 @@ int main(void){
 
     using namespace std::placeholders; //shorten the placeholders
     //placeholder _1 and _2 are used to bind the arguments to function in a specific order
+#ifdef PART_2
     std::function<int(int, int)> f1 = std::bind(Subtract, std::placeholders::_1, std::placeholders::_2);
     std::cout << "Order _1 - _2: " << f1(5, 3) << std::endl;
     f1 = std::bind(Subtract, _2, _1);
@@ -48,5 +52,37 @@ int main(void){
     //we have to bind the third argument to a constant value
     auto acc = std::bind(Accumulate, _1, _2, 0);
     std::cout << "Accumulate: " << Operation(1, 2, acc) << std::endl;
+#else /**
+    Lambda expressions can be used instead of std::bind. Is more readable and easier to understand.
+    */
+    //std::function<int(int, int)> f1 = std::bind(Subtract, std::placeholders::_1, std::placeholders::_2);
+    //std::cout << "Order _1 - _2: " << f1(5, 3) << std::endl;
+    auto f1 = [](int x, int y){ //std::bind(Subtract, _2, _1);
+        return Subtract(x , y);
+    };
+    std::cout << "Order _2 - _1: " << f1(5, 3) << std::endl;
+
+    //if you want to pass just one argument to the function, you can bind the second value to a constant value
+    auto f2 = [](auto && x){ //std::bind(Subtract, _1, 10);
+        return Subtract(std::forward<decltype(x)>(x), 10); 
+    };
+    std::cout << "Subtract 10 from: " << f2(2) << std::endl;
+
+    //taking another function to bind 
+    //std::function<bool(int, int)> f3 = std::bind(GreaterThan{}, _1, _2);
+    //typname T is just with C++20 possible
+    auto f3 = []<typename T>(T&& x, T&& y){
+        return GreaterThan{}(std::forward<T>(y), std::forward<T>(x));
+    };
+    std::cout << "Is greater than?: " << f3(32, 12) << std::endl;
+
+    //for what is std::bind useful? To use Accumulate function with only 2 arguments instead of 3
+    //we have to bind the third argument to a constant value
+    //auto acc = std::bind(Accumulate, _1, _2, 0);
+    auto acc = [](int x, int y){
+        return Accumulate(x, y, 0);
+    };
+    std::cout << "Accumulate: " << Operation(1, 2, acc) << std::endl;
+#endif
     return 0;
 }
