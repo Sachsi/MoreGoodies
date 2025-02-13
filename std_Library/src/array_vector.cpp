74513b5c93ba28ac3945a5a1cba283ae233b09f5
Tobias Sachse <tobias.sachse@rohde-schwarz.com>
2025-02-13 11:20:30 +0100

    vector step

--- std_Library/src/array_vector.cpp
+++ std_Library/src/array_vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 void Array(){
     std::array<int, 5> arr = {1, 3 ,5 ,6, 7};
@@ -19,8 +20,33 @@ void Array(){
 
 }
 
+void Vecoter(){
+    std::vector<int> coll{1, 2, 4, 5 ,6 };
+    for(int i = 0; i < 5; ++i)
+    {
+        coll.push_back(i);
+        std::cout << "size: " << coll.size() << std::endl;
+    }
+    coll[0] = 700;
+
+    auto itr = coll.begin();
+    while( itr != coll.end()){
+        std::cout << *itr++ << std::endl;
+    }
+
+    coll.insert(coll.begin() + 1 , 100);
+    coll.erase(coll.end() - 7);
+    coll.pop_back(); //remove the last element
+
+    auto itr1 = coll.begin();
+    while( itr1 != coll.end()){
+        std::cout << *itr1++ << std::endl;
+    }
+}
+
 int main(void)
 {
     Array();
+    Vecoter();
     return 0;
 }
