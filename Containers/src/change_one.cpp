390b31d04991d865fb33679d888f9b2ba347ea66
Tobias Sachse <tobias.sachse@rohde-schwarz.com>
2025-03-19 07:51:26 +0100

    change two without noexeption

--- Containers/src/change_one.cpp
+++ Containers/src/change_one.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 
-int main(void){
+void Emplace()
+{
     std::vector<Integer> vInt;
     Integer val{1};
     //vInt.push_back(val);
@@ -12,6 +13,15 @@ int main(void){
     data.push_back(std::pair<int, std::string>(1, "one"));
     //siehe the difference between push_back and emplace_back
     data.emplace_back(2, "two");
+}
+
+int main(void){
+
+    //Emplace();
+    std::vector<Integer> vInt;
+    vInt.push_back(1);
+    vInt.push_back(2);
+    vInt.push_back(5);
 
     return 1;
 }
