7a351d9e91b2baa278926771570c2d47c71d6c5d
Tobias Sachse <tobias.sachse@rohde-schwarz.com>
2025-05-03 11:38:56 +0200

    optional STEP3

--- std_Library/src/optional.cpp
+++ std_Library/src/optional.cpp
@@ -27,9 +27,25 @@ std::optional<const char*> GetErrorStringOp(int errorNo){
     }
     
 }
+class ContactName{
+    std::string name;
+    std::optional<std::string> middle;
+    std::string last;
 
+public:
+    ContactName(const std::string &f, const std::optional<std::string>& m, const std::string &la)
+    : name{f}, middle{m}, last{la}{
+        
+    }
+    void Print(){
+        std::cout << name << " ";
+        std::cout << middle.value_or("") << " ";
+        std::cout << last << std::endl;
+    }
+};
 int main(void) 
 {
+#ifdef OPTIONAL_STEP1
     //std::optional value{5}; //create an optional value with value 5, the type is deduced from the value
     auto value = std::make_optional(5); //create an optional value. 
     //specifie the type of the optional value
@@ -55,7 +71,7 @@ int main(void)
     opt = 4; //assign a new value to the optional value
     std::cout << "opt is not empty: " << opt.has_value() << std::endl;
     opt.reset(); //reset the optional value, it will be empty now
-    
+#endif //STEP1
 
 #ifdef OPTIONAL_STEP2
     try
@@ -91,6 +107,38 @@ int main(void)
         
 #endif //SEP 2
 
+//#ifdef OPTIONAL_STEP3
+    ContactName contact{"Tobias", std::nullopt, "Sachse"};
+    ContactName Wife{"Daniela", "Mendoza", "Escobar"};
+    contact.Print();
+    Wife.Print();
+    //std::optional value{5}; //create an optional value with value 5, the type is deduced from the value
+    auto value = std::make_optional(5); //create an optional value. 
+    //specifie the type of the optional value
+    //std::optional<int> value;
+    //check if the value is empty or not
+    if(value.has_value()){
+        std::cout << "value is: " << value.value() << std::endl;
+    }
+    //check if it is empty, and call it by refernece
+    if(value){
+        std::cout << "value ref is: " << *value << std::endl;
+    }
+
+    auto message = GetErrorString(1);
+    if(strlen(message) != 0){
+        std::cout << message << std::endl;
+    }else{
+        std::cout << "Unknown error number\n";
+    }
+
+    std::optional<int> opt{3};
+
+    opt = 4; //assign a new value to the optional value
+    std::cout << "opt is not empty: " << opt.has_value() << std::endl;
+    opt.reset(); //reset the optional value, it will be empty now
+        
+//#endif //SEP 3
 
 
     return 1;
