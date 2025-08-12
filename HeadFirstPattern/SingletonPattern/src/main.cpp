d52cdb4e30c77589f1d2669a9f12458adb3d6f5e
Sachsi <tobisachse27@msn.com>
2025-08-12 22:25:04 +0200

    create framework

---
+++ HeadFirstPattern/SingletonPattern/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to the Singleton Pattern example!" << std::endl;
+
+    // Future implementation will go here
+    // For example, you could create a Singleton class and demonstrate its usage
+
+    return 0;
+}
