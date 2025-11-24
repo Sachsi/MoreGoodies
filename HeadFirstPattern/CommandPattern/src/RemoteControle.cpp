f660712f9965a7db1dd669157be5f3451e3b727d
Sachsi <tobisachse27@msn.com>
2025-11-24 22:40:11 +0100

    created Methods for On/Off Buttons in RemoteControle

--- HeadFirstPattern/CommandPattern/src/RemoteControle.cpp
+++ HeadFirstPattern/CommandPattern/src/RemoteControle.cpp
@@ -33,5 +33,31 @@ namespace RemoteControle
         
         std::cout << "RemoteControle with 7 slots created" << std::endl;
     }
+    void RemoteControl::setCommand(int slot, CommandInterface* onCommand, CommandInterface* offCommand)
+    {
+        this->onCommand[slot] = onCommand;
+        this->offCommand[slot] = offCommand;
+    }
+    void RemoteControl::onButtonWasPushed(int slot)
+    {
+        this->onCommand[slot]->execute();
+    }
+    void RemoteControl::offButtonWasPushed(int slot)
+    {
+        this->offCommand[slot]->execute();
+    }
+    std::string RemoteControl::toString()
+    {
+        std::string stringBuffer;
+        stringBuffer.append("\n------ Remote Control -------\n");
+        for (int i = 0; i < 7; i ++){
+            stringBuffer.append("[slot " + std::to_string(i) + "] ");
+            stringBuffer.append(typeid(onCommand[i]).name());
+            stringBuffer.append("   ");
+            stringBuffer.append(typeid(offCommand[i]).name());
+            stringBuffer.append("\n");
+        }
+        return stringBuffer;
+    }
 
 } // namespace RemoteControle
