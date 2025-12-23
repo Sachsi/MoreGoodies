18aa5bec5635ede1fbea59a83cf6e6527829385c
Sachsi <tobisachse27@msn.com>
2025-12-23 14:22:31 +0100

    Overflow data

---
+++ DataConvertion/OverUnderflow/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main(){
+
+    //Overflow
+    unsigned char data {250};
+
+    ++data;
+    std::cout << "data value: " << static_cast<int>(data) << '\n';
+
+    ++data;
+    std::cout << "data value: " << static_cast<int>(data) << '\n';
+    ++data;
+    std::cout << "data value: " << static_cast<int>(data) << '\n';
+    ++data;
+    std::cout << "data value: " << static_cast<int>(data) << '\n';
+    ++data;
+    std::cout << "data value: " << std::hex << static_cast<int>(data) << '\n'; //0xff
+
+    std::cout << "Overflow value: " << std::dec << static_cast<int>(data) << '\n'; //255
+    ++data;
+    std::cout << "Overflowed value: " << static_cast<int>(data) << '\n'; //0
+
+
+    return 0;
+}
