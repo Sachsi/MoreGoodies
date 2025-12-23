e631421105ca9e9c63c58710925edb3e7523bb27
Sachsi <tobisachse27@msn.com>
2025-12-23 14:22:47 +0100

    underflow data

--- DataConvertion/OverUnderflow/main.cpp
+++ DataConvertion/OverUnderflow/main.cpp
@@ -22,6 +22,14 @@ int main(){
     ++data;
     std::cout << "Overflowed value: " << static_cast<int>(data) << '\n'; //0
 
+    //Underflow
+    std::cout << "------------------\n";
+    data = 1;
+    std::cout << "data value: " << static_cast<int>(data) << '\n';
+    --data;
+    std::cout << "data value: " << static_cast<int>(data) << '\n';
+    --data;
+    std::cout << "Underflowed value: " << static_cast<int>(data) << '\n'; //255
 
     return 0;
 }
